139e387083d52c742ca3263dc089766b58ac0c17
niccokunzmann <niccokunzmann@gmx.de>
2017-04-27 20:44:42 +0000

    Generated code by SwaggerHub

---
+++ python-flask/controllers/ressource_controller.py
@@ -0,0 +1,15 @@
+
+def add_ressources(body) -> str:
+    return 'do some magic!'
+
+def delete_ressource(ressourceId) -> str:
+    return 'do some magic!'
+
+def delete_ressources() -> str:
+    return 'do some magic!'
+
+def get_ressource(ressourceId) -> str:
+    return 'do some magic!'
+
+def get_ressources($skip = None, $limit = None) -> str:
+    return 'do some magic!'
