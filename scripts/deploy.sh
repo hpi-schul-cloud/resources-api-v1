ff24ea4a9bfe8d2b7d1ac82b63dcd52fd59f63f5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-28 14:06:38 -0700

    adding credentials for pypi upload

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 cd "`dirname \"$0\"`"
 
-dir="generators/python_client/dist"
+dir="../generators/python_client/dist"
 if ! [ -d "$dir" ]; then
   echo "Directory $dir does not exist but is expected to exist."
   exit 1
