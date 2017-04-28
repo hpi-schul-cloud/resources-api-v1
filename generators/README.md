697f36fef53b86e09159ebeb3347ab437b6a76b1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-28 12:32:12 -0700

    python client code generator

---
+++ generators/README.md
@@ -0,0 +1,23 @@
+# Generators
+
+These files generate the source code you can use as a base for your
+clients and servers.
+
+Generation uses the [swagger-codegen][swagger-codegen] repository.
+We generate
+
+- Python server
+- Python Client
+  - with upload to pypi
+- upload all generated code to the github releases
+
+
+## Installation
+
+Install Java:
+
+    sudo apt-get -y install default-jre python3-pip
+
+
+
+[swagger-codegen]: https://github.com/swagger-api/swagger-codegen 
---
+++ generators/generate_python_client.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+
+folder="output"
+version_ending=""
+config="python_client_config.json"
+if [ -n "$TRAVIS_BUILD_NUMBER" ]; then
+  # see https://docs.travis-ci.com/user/environment-variables/
+  version_ending="-build-$TRAVIS_BUILD_NUMBER-commit-${TRAVIS_COMMIT:0:7}"
+fi
+
+
+# config values from
+#   java -jar generators/swagger-codegen-cli-2.2.2.jar config-help -l python
+echo "{
+        \"packageName\": \"schul_cloud_ressource_api_v1\",
+        \"packageVersion\" : \"1.0.0$version_ending\",
+        \"packageUrl\" : \"https://github.com/schul-cloud/ressources-api-v1\"
+      }" > "$config"
+echo "Configutation:"
+cat "$config"
+echo
+./generate_code.sh python "$output" --config "$config"
+
+(
+  cd "$output"
+  python3 -m pip install --user -r requirements.txt \
+                                -r test-requirements.txt \
+                                wheel
+  python3 -m nosetests
+  python3 setup.py sdist bdist_wheel
+)
