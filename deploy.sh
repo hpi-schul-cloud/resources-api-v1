c5ce5b4629644d62e7526274a5c78b4f2347f2dc
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-28 13:53:32 -0700

    moved deploy logic

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd "`dirname \"$0\"`"
+
+dir="generators/python_client/dist"
+if ! [ -d "$dir" ]; then
+  echo "Directory $dir does not exist but is expected to exist."
+  exit 1
+fi
+
+twine upload "$dir"/*
