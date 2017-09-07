52b86cf6d21a5b03c81b36750a595301398cd9a5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-09-07 04:27:22 -0700

    add module to publish

---
+++ generators/node/generate-package.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Followed this tutorial to generate the package:
+#    https://www.hacksparrow.com/how-to-write-node-js-modules.html
+#
+
+set -e
+
+cd "`dirname \"$0\"`"
+
+config="./schul-cloud-resources-api-v1/package.json"
+version_ending=""
+if [ -n "$TRAVIS_BUILD_NUMBER" ]; then
+  # see https://docs.travis-ci.com/user/environment-variables/
+  version_ending=".${TRAVIS_BUILD_NUMBER}"
+fi
+version="1.0.0$version_ending"
+
+# see
+#    https://docs.npmjs.com/files/package.json
+echo "{
+        \"name\": \"schul-cloud-resources-api-v1\",
+        \"version\" : \"$version\",
+        \"homepage\" : \"https://github.com/schul-cloud/resources-api-v1\",
+        \"description\" : \"The learning resources' api definitions for Schul-Cloud.\",
+        \"license\" : \"AGPL-1.0\",
+        \"private\" : false
+      }" > "$config"
+
+(
+  cd schul-cloud-resources-api-v1
+  npm publish
+)
