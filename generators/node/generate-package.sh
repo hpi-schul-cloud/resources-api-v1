1324502206084015607c110a4a7285985b5ce44e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-09-08 00:55:44 -0700

    adding the schema files

--- generators/node/generate-package.sh
+++ generators/node/generate-package.sh
@@ -8,8 +8,14 @@ set -e
 
 cd "`dirname \"$0\"`"
 
-config="./schul-cloud-resources-api-v1/package.json"
+output="./schul-cloud-resources-api-v1"
+config="$output/package.json"
 version="`../scripts/version`"
+schemas="$output/schemas"
+
+echo "# Copy schemas to package"
+
+../scripts/copy_schemas_to "$schemas"
 
 # see
 #    https://docs.npmjs.com/files/package.json
@@ -20,9 +26,29 @@ echo "{
         \"description\" : \"The learning resources' api definitions for Schul-Cloud.\",
         \"license\" : \"AGPL-1.0\",
         \"private\" : false,
-        \"publishConfig\" : { \"access\" : \"public\" }
+        \"publishConfig\" : { \"access\" : \"public\" },
+        \"files\" : [
+`
+          for file in \`( cd "$output" && find schemas -name \*.json )\`; do
+            echo "          \\\"$file\\\","
+          done
+        `
+          \"LICENSE\",
+          \"schul-cloud-resources-api-v1.js\",
+          \"README.md\"
+        ]
       }" > "$config"
 
-../scripts/copy_schemas_to "schul-cloud-resources-api-v1/schemas"
+echo "# Config: "
+cat "$config"
+echo
+
+cp ../../LICENSE "$output" 
 
 # TODO: test the package
+
+(
+  cd "$output"
+  echo "# Generating package: "
+  npm pack
+)
